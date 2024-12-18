453d4026e74b3a3124949ae804b07e29af8e12f0
David Morales <damoralesr97@outlook.com>
2024-12-18 10:35:28 -0500

    add mongo datasource

--- README.md
+++ README.md
@@ -7,7 +7,15 @@
 MAILER_SERVICE=gmail
 MAILER_EMAIL=
 MAILER_SECRET_KEY=
+MONGO_URL=mongodb://dmorales:123456@localhost:27017
+MONGO_DB_NAME=NOC
+MONGO_USER=dmorales
+MONGO_PASS=123456
 ```
+
+### Obtener Gmail key
+[Google AppPassword](https://myaccount.google.com/u/0/apppaswords)
+
 3. Ejecutar el comando ```npm install```
 
 4. Ejecutar ```npm run dev```
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,23 +1,30 @@
+import { LogSeverityLevel } from "../domain/entities/log.entity";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
+import { MongoDataSource } from "../infrastructure/datasources/mongo.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 
-const fileSystemLogRepository = new LogRepositoryImpl(
+const logRepository = new LogRepositoryImpl(
+  // new MongoDataSource()
   new FileSystemDataSource()
 );
 
 export class Server {
-  public static start() {
+  public static async start() {
     console.log("Server started...");
 
-    CronService.createJob("*/5 * * * * *", () => {
-      const url = "http://localhost:3000";
-      new CheckService(
-        fileSystemLogRepository,
-        () => console.log(`${url} is ok`),
-        (error) => console.log(error)
-      ).execute(url);
-    });
+    const logs = await logRepository.getLogs(LogSeverityLevel.high);
+    console.log(logs);
+
+    // CronService.createJob("*/5 * * * * *", () => {
+    //   // const url = "http://localhost:3000";
+    //   const url = "https://www.google.com";
+    //   new CheckService(
+    //     logRepository,
+    //     () => console.log(`${url} is ok`),
+    //     (error) => console.log(error)
+    //   ).execute(url);
+    // });
   }
 }
