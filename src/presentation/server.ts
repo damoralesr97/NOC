0835c757f24638e5b00f1f3af877aeebb893f203
David Morales <damoralesr97@outlook.com>
2024-12-18 20:00:41 -0500

    check service with multiple sources

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,32 +1,26 @@
-import { LogSeverityLevel } from "../domain/entities/log.entity";
-import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CheckServiceMultiple } from "../domain/use-cases/checks/check-service-multiple";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { MongoDataSource } from "../infrastructure/datasources/mongo.datasource";
 import { PostgresDataSource } from "../infrastructure/datasources/postgres.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 
-const logRepository = new LogRepositoryImpl(
-  // new MongoDataSource()
-  // new FileSystemDataSource()
-  new PostgresDataSource()
-);
+const fsLogRepository = new LogRepositoryImpl(new FileSystemDataSource());
+const monogLogRepository = new LogRepositoryImpl(new MongoDataSource());
+const postgresqlLogRepository = new LogRepositoryImpl(new PostgresDataSource());
 
 export class Server {
   public static async start() {
     console.log("Server started...");
 
-    // const logs = await logRepository.getLogs(LogSeverityLevel.high);
-    // console.log(logs);
-
-    // CronService.createJob("*/5 * * * * *", () => {
-    //   // const url = "http://localhost:3000";
-    //   const url = "https://www.google.com";
-    //   new CheckService(
-    //     logRepository,
-    //     () => console.log(`${url} is ok`),
-    //     (error) => console.log(error)
-    //   ).execute(url);
-    // });
+    CronService.createJob("*/5 * * * * *", () => {
+      // const url = "http://localhost:3000";
+      const url = "https://www.google.com";
+      new CheckServiceMultiple(
+        [fsLogRepository, monogLogRepository, postgresqlLogRepository],
+        () => console.log(`${url} is ok`),
+        (error) => console.log(error)
+      ).execute(url);
+    });
   }
 }
