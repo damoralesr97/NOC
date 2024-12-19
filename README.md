3bbb94b577f7e5ce88e6e588929e65ba5c9048af
David Morales <damoralesr97@outlook.com>
2024-12-18 19:50:51 -0500

    add postgresql datasource

--- README.md
+++ README.md
@@ -3,19 +3,18 @@
 ## dev
 1. Clonar el archivo .env.template a .env
 2. Configurar las variables de entorno
+3. Ejecutar el comando 
 ```
-MAILER_SERVICE=gmail
-MAILER_EMAIL=
-MAILER_SECRET_KEY=
-MONGO_URL=mongodb://dmorales:123456@localhost:27017
-MONGO_DB_NAME=NOC
-MONGO_USER=dmorales
-MONGO_PASS=123456
+npm install
+```
+4. Ejecutar las bases de datos con el comando
+```
+docker compose up -d
+```
+5. Ejecutar 
+```
+npm run dev
 ```
 
 ### Obtener Gmail key
 [Google AppPassword](https://myaccount.google.com/u/0/apppaswords)
-
-3. Ejecutar el comando ```npm install```
-
-4. Ejecutar ```npm run dev```
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,20 +2,22 @@ import { LogSeverityLevel } from "../domain/entities/log.entity";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { MongoDataSource } from "../infrastructure/datasources/mongo.datasource";
+import { PostgresDataSource } from "../infrastructure/datasources/postgres.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 
 const logRepository = new LogRepositoryImpl(
   // new MongoDataSource()
-  new FileSystemDataSource()
+  // new FileSystemDataSource()
+  new PostgresDataSource()
 );
 
 export class Server {
   public static async start() {
     console.log("Server started...");
 
-    const logs = await logRepository.getLogs(LogSeverityLevel.high);
-    console.log(logs);
+    // const logs = await logRepository.getLogs(LogSeverityLevel.high);
+    // console.log(logs);
 
     // CronService.createJob("*/5 * * * * *", () => {
     //   // const url = "http://localhost:3000";
